c9fc418ddd8feb674ebc28976fd49b3600ed6fee
xiankai <161225482+sjdcwejf@users.noreply.github.com>
2026-06-24 17:12:02 +0800

    Remove unused template UI

--- tiny_wizard/settings/game_settings.gd
+++ tiny_wizard/settings/game_settings.gd
@@ -49,8 +49,8 @@ const UI_TEXT := {
 		"en": "Quit Game",
 	},
 	"version": {
-		"zh": "原型构建 0.1.62",
-		"en": "Prototype Build 0.1.62",
+		"zh": "原型构建 0.1.63",
+		"en": "Prototype Build 0.1.63",
 	},
 	"settings_title": {
 		"zh": "设置",
